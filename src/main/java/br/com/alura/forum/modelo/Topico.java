45c8cbc52178daa64a94b762aed5c51238d62960
Leandro Martins <leosonic2@gmail.com>
2021-03-13 17:17:06 -0300

    Adicionado método POST para os Tópicos

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -40,9 +40,11 @@ public class Topico {
 	}
 
 	public Topico(String titulo, String mensagem, Curso curso) {
+		super();
 		this.titulo = titulo;
 		this.mensagem = mensagem;
 		this.curso = curso;
+
 	}
 
 	@Override
