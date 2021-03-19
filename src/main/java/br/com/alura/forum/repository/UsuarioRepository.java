f0003624f870772316bd0aefc658201ab887546f
Leandro Martins <leosonic2@gmail.com>
2021-03-19 16:18:48 -0300

    adicionado proteção com spring security

--- src/main/java/br/com/alura/forum/repository/UsuarioRepository.java
+++ src/main/java/br/com/alura/forum/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Usuario;
@@ -8,4 +10,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Usuario findByNome(String nome);
 
+	public Optional<Usuario> findByEmail(String email);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$qnm5mNpA.c3pdo6.6rnaPuf/6VipstaucvFUfV/XtRCZXQzN8.bs.');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
